721b7406f7a75e98a56cc5f7aa4f6d5b8f145e8e
Mike Cardwell <mcardwell@centiq.co.uk>
2016-10-05 12:58:51 +0100

    Bump version to 1.0.13

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'centiq:crud',
-  version: '1.0.12',
+  version: '1.0.13',
   summary: 'Apply crud operations across meteor methods publications and http.',
   git: 'https://github.com/Centiq/meteor-crud',
   documentation: 'README.md'
